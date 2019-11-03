0130e9232363729d194c95f11dee28d97a3fb1f8
siromath <sirosiromath@gmail.com>
2019-11-03 20:30:19 +0900

    Update "createAttrString" function

--- src/functions/createAttrString.ts
+++ src/functions/createAttrString.ts
@@ -8,7 +8,23 @@ export function createAttrString (attrs: FrontJAttrsObject = {}): string {
   return attrNames.map((attrName) => {
     // attrs[attrName]だと型推論が効かなかったので一旦代入する
     const _attrValue = attrs[attrName]
-    const attrValue = Array.isArray(_attrValue) ? _attrValue.join(' ') : _attrValue
+    let attrValue: string
+
+    // 属性の値が空文字の場合は属性名のみ返す(checked属性などを想定)
+    if (_attrValue === '') {
+      return `${attrName}`
+    }
+
+    if (Array.isArray(_attrValue)) {
+      // 属性の値が空配列の場合も属性名のみ返す
+      if (_attrValue.length === 0) {
+        return `${attrName}`
+      }
+      attrValue = _attrValue.join(' ')
+    } else {
+      attrValue = _attrValue
+    }
+
     return `${attrName}="${attrValue}"`
   }).join(' ')
 }
