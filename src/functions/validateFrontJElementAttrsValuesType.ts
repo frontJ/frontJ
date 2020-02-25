12c22adaa6752b3560b01870e8cd526a1b4f5819
siromath <sirosiromath@gmail.com>
2020-02-25 16:37:04 +0900

    Update Error messages

--- src/functions/validateFrontJElementAttrsValuesType.ts
+++ src/functions/validateFrontJElementAttrsValuesType.ts
@@ -1,6 +1,6 @@
 export function validateFrontJElementAttrsValuesType (values: unknown[]) {
   // find()では「undefinedが見つかった場合」と「対象が見つからなかった場合」の返り値がどちらもundefinedとなるため、filter()のlengthを調べる
   if (values.filter((value) => typeof value !== 'string' && typeof value !== 'number').length) {
-    throw new Error('Attrs values are must be string or number.')
+    throw new Error('attrs values are must be string or number.')
   }
 }
--- src/functions/validateFrontJElementContentsType.ts
+++ src/functions/validateFrontJElementContentsType.ts
@@ -1,6 +1,6 @@
 export function validateFrontJElementContentsType (contents: unknown[]) {
   // find()では「undefinedが見つかった場合」と「対象が見つからなかった場合」の返り値がどちらもundefinedとなるため、filter()のlengthを調べる
   if (contents.filter((content) => typeof content !== 'string' && typeof content !== 'number').length) {
-    throw new Error('Contents are must be string or number.')
+    throw new Error('contents are must be string or number.')
   }
 }
