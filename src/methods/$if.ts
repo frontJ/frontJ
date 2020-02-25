f91ad60173898a433f42262bea3fb1e322759467
siromath <sirosiromath@gmail.com>
2020-02-25 16:42:48 +0900

    Update return part of "$if" function

--- src/methods/$if.ts
+++ src/methods/$if.ts
@@ -3,9 +3,5 @@ import { validate$ifArgumentsType } from '../functions'
 export function $if (condition: boolean, value: string | number, elseValue: string | number = ''): string | number {
   validate$ifArgumentsType(condition, value, elseValue)
 
-  if (condition) {
-    return value
-  } else {
-    return elseValue
-  }
+  return condition ? value : elseValue
 }
